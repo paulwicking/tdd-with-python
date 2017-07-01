6aef9006965d32f8af6214b2e462cedafc09d4dc
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-01 11:28:02 +0200

    Redirect after POST, show all items in template.

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,7 @@
 from django.test import TestCase
 from lists.models import Item
 
+
 class HomePageTest(TestCase):
     """Test the home page.
 
@@ -20,9 +21,43 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
+        """Check that we store list items.
+
+        :return:
+        """
+        self.client.post('/', data={'item_text': 'A new list item'})
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+    def test_redirects_after_post(self):
+        """Check that we redirect back to home page after storing list items.
+
+        """
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
+    def test_only_saves_items_when_necessary(self):
+        """Make sure that empty requests are not stored in the database.
+
+        """
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        """Checks that all list items display correctly.
+
+
+        """
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
 
 
 class ItemModelTest(TestCase):

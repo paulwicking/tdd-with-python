c0e8c1e1c083d0b7a4bdf13433c1854ea3645bf3
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-01 09:44:13 +0200

    Moved statements for PEP8 compliance, acc. pylint.

--- functional_tests.py
+++ functional_tests.py
@@ -3,10 +3,11 @@
 Obey the Testing Goat.
 
 """
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -47,8 +48,8 @@ class NewVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-to item did not appear in table")
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows), "New to-to item did not appear in table")
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly". Edith is very methodical.

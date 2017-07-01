3f380a6778cd3baaa75d58f7ad8aa9c2ca88fc4d
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-01 10:24:09 +0200

    Adding helper function, test still failing as expected.

--- functional_tests.py
+++ functional_tests.py
@@ -20,6 +20,16 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """Checks for text in table rows.
+
+        :argument row_text: ``str``
+
+        """
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """Creates multiple entries in a list and checks the retrieval url.
 
@@ -45,10 +55,7 @@ class NewVisitorTest(unittest.TestCase):
         # "1: Buy peacock feathers" as the item in a to-do list.
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item.
         # She enters "Use peacock feathers to make a fly". Edith is very methodical.

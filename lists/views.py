4f5abbc94061c0bd721fa4faea1500eb5ec81b38
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-01 17:35:44 +0200

    Form URL change, moved code, new URL.
    Forms in home and list now use /lists/new as action.
    Moved code in views and tests.
    Added new url.

--- lists/views.py
+++ lists/views.py
@@ -3,16 +3,25 @@ from lists.models import Item
 
 
 def home_page(request):
-    """Renders the content on the root home page and stores data as required.
+    """Renders the front page.
 
     """
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')
 
 
 def view_list(request):
+    """Checks that we can view a list.
+
+    """
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})
+
+
+def new_list(request):
+    """Checks that we can create a new list.
+
+    :param request: http request
+    :return: http redirects or assertion failure.
+    """
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')

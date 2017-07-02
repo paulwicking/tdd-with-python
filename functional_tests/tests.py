0ff5b0b6aa88f9af4d81a5c75253db9c8f29871c
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-02 19:58:36 +0200

    Hacking FT to allow test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -3,6 +3,7 @@
 Obey the Testing Goat.
 
 """
+import os
 import time
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -19,6 +20,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     """
     def setUp(self):
         self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         self.browser.quit()

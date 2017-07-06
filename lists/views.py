efdce598f0ebfdeeca69116b9259c23180e26c75
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-06 21:30:15 +0200

    Adjust new list view to do model validation.

--- lists/views.py
+++ lists/views.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from lists.models import Item, List
 
@@ -27,7 +28,14 @@ def new_list(request):
     :return: http redirects or assertion failure.
     """
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST['item_text'], list=list_)
+    item = Item(text=request.POST['item_text'], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
 
 

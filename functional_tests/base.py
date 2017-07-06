5e452868c1046cbe89e90ac45026748d07215019
Paul Wicking <wowsuchnamaste@users.noreply.github.com>
2017-07-06 19:05:13 +0200

    REFACTOR: Moved FTs into individual files.

---
+++ functional_tests/base.py
@@ -0,0 +1,48 @@
+"""Functional test for an example to-do website.
+
+Obey the Testing Goat.
+
+"""
+import os
+import time
+from unittest import skip
+
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from selenium import webdriver
+
+from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 5
+
+
+class FunctionalTest(StaticLiveServerTestCase):
+    """Base test class.
+
+    """
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        staging_server = os.environ.get('STAGING_SERVER')
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def wait_for_row_in_list_table(self, row_text):
+        """Checks for text in table rows after reload. Time-out value in MAX_WAIT constant.
+
+        :argument row_text: ``str``
+
+        """
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as error:
+                if time.time() - start_time > MAX_WAIT:
+                    raise error
+                time.sleep(0.5)
